1a3acb818a2db71374c90d75f3d0fe130838bf02
Gabriel Monteiro <gmonteeeiro@gmail.com>
2019-02-03 23:58:49 -0200

    projeto Database

--- csharp-sqlite/Form1.cs
+++ csharp-sqlite/Form1.cs
@@ -6,14 +6,29 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using Database;
 
 namespace csharp_sqlite
 {
     public partial class Form1 : Form
     {
+        private Db db;
+
         public Form1()
         {
             InitializeComponent();
+
+            db = new Db();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            db.ExecuteNonQuery(richTextBox1.Text);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
